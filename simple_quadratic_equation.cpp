73909db0f7201eca3c551f62f475f8d7d349a67a
Hubert Mucyo <h.mucyo@alustudent.com>
2024-06-22 22:06:36 +0530

    discriminant

--- simple_quadratic_equation.cpp
+++ simple_quadratic_equation.cpp
@@ -2,11 +2,10 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int main(){
-	int a,b,c;
-	float root1=0,root2=0,delta=0;
-	cout<<"Enter three coefficients of your equation:\n";
-	cin>>a>>b>>c;
+
+float root1=0,root2=0,delta=0;
+
+void discriminant(){
 	delta=sqrt((b*b)/(4*a*c));
 	if (delta>0){
 		root1=(-b+delta)/(2*a);
@@ -14,14 +13,24 @@ int main(){
 		cout<<"1st root: "<<root1<<endl;	
 		cout<<"2nd root: "<<root2;
 	}
-	else if(delta=0){
+	else if(delta==0){
 		root1=(-b)/(2*a);
 		root2=root1;
 		cout<<"1st root: "<<root1<<endl;
 		cout<<"2nd root: "<<root2;
 	}
-	else{
-		cout<<"Error!!!!!!!\n";
+	else if(delta<0){
+		cout<<"1st root: "<<(-b/2*a)<<"+i"<<(sqrt(-(b*b-(4*a*c)))/2*a)<<endl;
+		cout<<"2nd root: "<<(-b/2*a)<<"-i"<<(sqrt(-(b*b-(4*a*c)))/2*a)<<endl;
 	}
+
+}
+
+int main(){
+	int a,b,c;
+	cout<<"Enter three coefficients of your equation:\n";
+	cin>>a>>b>>c;
+	discriminant();
+	
 	return 0;
 }
