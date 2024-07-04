83e88310d100ecf891a4ba89a978a79750dd494a
Hubert Mucyo <h.mucyo@alustudent.com>
2024-07-04 22:52:13 +0530

    prime!!

--- prime_interval.cpp
+++ prime_interval.cpp
@@ -1,17 +1,37 @@
-//program to print all prime numbers between two intervals
-#include<iostream>
+#include <iostream>
+#include <cmath> // for sqrt function
+
 using namespace std;
-int main(){
-	int num1,num2;
-	bool prime=true;
-	cout<<"Enter 2 numbers in ascending order!\n";
-	cin>>num1>>num2;
-	for( ;num1<num2;num1++){
-		if(num2%num1==0){
-			prime=false;
-		}
-		else{
-			cout<<num1;
-		}
-	}
+
+int main() {
+    int num1, num2;
+    prompt:
+    cout << "Enter 2 numbers! [The first should be smaller!]\n";
+    cin >> num1 >> num2;
+
+    if (num1 > num2) {
+        cout << "The first number should be smaller than the second number.\n";
+        goto prompt;
+    }
+
+    cout << "The prime numbers between " << num1 << " and " << num2 << " are:\n";
+    for (int i = num1; i <= num2; i++) {
+        if (i <= 1) continue; // Skip numbers less than or equal to 1
+
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
 }
+
