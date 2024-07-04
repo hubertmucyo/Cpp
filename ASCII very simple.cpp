e6a4022c24efa62a857065e864bb9cf81352232e
Hubert Mucyo <h.mucyo@alustudent.com>
2024-07-04 22:08:33 +0530

    ASCII

---
+++ ASCII very simple.cpp
@@ -0,0 +1,14 @@
+//a C++ Program to Find ASCII Value of a Character.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char ch;
+	cout<<"Enter a character:\n";
+	cin>>ch;
+	cout<<"The ASCII value is: \n";
+	cout<<int(ch);
+	
+}
